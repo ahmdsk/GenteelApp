06d581dac3dd67f754745847753c452cc148457f
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-10-27 15:23:27 +0700

    fix homescreen

--- lib/screen/homepage_screen.dart
+++ lib/screen/homepage_screen.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:icons_flutter/icons_flutter.dart';
 
 const List<String> genderList = ['Women', 'Men', 'Children'];
+
 class HomepageScreen extends StatefulWidget {
   const HomepageScreen({super.key});
 
@@ -17,35 +18,46 @@ class _HomepageScreenState extends State<HomepageScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: DropdownButton(
-          value: dropDownValue.toUpperCase(),
-          style: GoogleFonts.lora(
-            fontSize: 24.0,
-            fontWeight: FontWeight.w500,
-            color: Colors.black,
-          ),
-          elevation: 0,
-          icon: const Icon(Feather.chevron_down),
-          items: genderList.map((gender) {
-            return DropdownMenuItem(
-              value: gender.toUpperCase(),
-              alignment: Alignment.center,
-              child: Text(
-                gender,
-                style: GoogleFonts.lora(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black,
-                ),
+        title: SizedBox(
+          width: 120.0,
+          child: DropdownButtonFormField(
+            value: dropDownValue,
+            style: GoogleFonts.lora(
+              fontSize: 24.0,
+              fontWeight: FontWeight.w500,
+              color: Colors.black,
+            ),
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(
+                borderSide: BorderSide.none,
               ),
-            );
-          }).toList(),
-          onChanged: (value) {
-            setState(() {
-              dropDownValue = value!;
-            });
-          },
+            ),
+            elevation: 0,
+            icon: const Icon(
+              Feather.chevron_down,
+              size: 18.0,
+            ),
+            items: genderList.map((gender) {
+              return DropdownMenuItem(
+                value: gender,
+                alignment: Alignment.center,
+                child: Text(
+                  gender.toUpperCase(),
+                  style: GoogleFonts.lora(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+              );
+            }).toList(),
+            onChanged: (value) {
+              debugPrint("valuenya: $value");
+              setState(() {
+                dropDownValue = value!;
+              });
+            },
+          ),
         ),
         actions: [
           IconButton(icon: const Icon(Feather.heart), onPressed: () {}),
@@ -55,8 +67,10 @@ class _HomepageScreenState extends State<HomepageScreen> {
       body: ListView(
         children: [
           Container(
-            margin:
-                const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+            margin: const EdgeInsets.symmetric(
+              horizontal: 18.0,
+              vertical: 16.0,
+            ),
             color: const Color(0xFFF7F7F7),
             child: Row(children: [
               IconButton(icon: const Icon(Feather.search), onPressed: () {}),
